3c212595df1c344bc4414e9829ac8a7870e855de
Joeky <joeky5888@gmail.com>
2023-09-06 11:36:55 +0800

    Use tokio::time
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,42 @@
 /*
-    Ref 1. https://github.com/tokio-rs/tokio/discussions/2648
-    Ref 2. https://stackoverflow.com/a/67650171
-    Ref 3. https://stackoverflow.com/a/73336289
- */
+   Ref 1. https://github.com/tokio-rs/tokio/discussions/2648
+   Ref 2. https://stackoverflow.com/a/67650171
+   Ref 3. https://stackoverflow.com/a/73336289
+*/
 
-use std::{collections::HashMap, error::Error, time, thread, sync::Arc};
+use std::{collections::HashMap, error::Error, sync::Arc, time::Duration};
 
 use futures::stream::FuturesUnordered;
-use tokio::sync::Semaphore;
+use tokio::{sync::Semaphore, time::sleep};
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
     let mut map: HashMap<String, String> = HashMap::<String, String>::with_capacity(10);
 
-    map.insert("1".to_string(), "1".to_string());
-    map.insert("2".to_string(), "2".to_string());
-    map.insert("3".to_string(), "3".to_string());
-    map.insert("4".to_string(), "4".to_string());
+    for i in 0..9999 {
+        map.insert(i.to_string(), i.to_string());
+    }
+
     println!("{}", map.len());
 
-    let max_concurrent: usize= 1;
+    let max_concurrent: usize = 999;
     let async_pool = Arc::new(Semaphore::new(max_concurrent));
 
     let tasks = map
-    .iter()
-    .map(|(key, val)| {
-        let key = key.clone();
-        let val = val.clone();
-        let acquire_pool = Arc::clone(&async_pool).acquire_owned();
+        .iter()
+        .map(|(key, val)| {
+            let key = key.clone();
+            let val = val.clone();
+            let acquire_pool = Arc::clone(&async_pool).acquire_owned();
 
-        tokio::spawn(async move {
-            let _release_pool = acquire_pool.await;
-            println!("key: {key} val: {val}");
-            let ten_millis = time::Duration::from_millis(500);
-            thread::sleep(ten_millis);
+            tokio::spawn(async move {
+                let _release_pool = acquire_pool.await;
+                println!("key: {key} val: {val}");
+                sleep(Duration::from_millis(1000)).await;
+            })
         })
-    }).collect::<FuturesUnordered<_>>();
+        .collect::<FuturesUnordered<_>>();
 
     let errgroup = futures::future::join_all(tasks).await;
     println!("{:?}", errgroup);
